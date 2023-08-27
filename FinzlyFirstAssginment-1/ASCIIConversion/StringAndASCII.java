5d9cb2cc521c9b5911d72af95c184220292c0d42
Sateesh Kumbhakar <sateeshkumbhakar113@gmail.com>
2023-08-27 21:57:08 +0530

    coddig solution of ASCII conversion is added

--- FinzlyFirstAssginment-1/ASCIIConversion/StringAndASCII.java
+++ FinzlyFirstAssginment-1/ASCIIConversion/StringAndASCII.java
@@ -1,5 +1,27 @@
 package forexSystem;
 
+import java.util.Scanner;
+
 public class StringAndASCII {
+   
+    public static String ASCIIConversion(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i=0;i<str.length();i++) {
+        	char ch = str.charAt(i);
+            if (ch != ' ') {
+                result.append((int) ch);
+            } else {
+                result.append(' ');
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main (String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print(ASCIIConversion(s.nextLine()));
+
+    }
 
 }
